6c5ee6d1aff9d1e2456d41acab685da9b93685e1
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-01-28 14:03:07 +0200

    Changed font and window size

--- SciCalc/MauiProgram.cs
+++ SciCalc/MauiProgram.cs
@@ -11,8 +11,8 @@ namespace SciCalc
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Cairo-Light.ttf", "RegularFont");
+                    fonts.AddFont("Cairo-ExtraLight.ttf", "LightFont");
                 });
 
 #if DEBUG
