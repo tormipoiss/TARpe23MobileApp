55303e02f8bee26d599b707a1cf41fc1d2d28829
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-01-28 13:23:08 +0200

    Created .NET MAUI mobile app

---
+++ SciCalc/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SciCalc
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SciCalc/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SciCalc
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
