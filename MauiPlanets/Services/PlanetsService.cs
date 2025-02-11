90dca0d131789e83d3ba9b5c8e7a49a9598a1428
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-02-11 13:20:25 +0200

    added planet and a bit of planetservice

---
+++ MauiPlanets/Services/PlanetsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiPlanets.Models;
+
+namespace MauiPlanets.Services
+{
+    class PlanetsService
+    {
+
+        private static List<Planet> planets = new()
+        {
+            new()
+            {
+
+            }
+        };
+    }
+}
--- MauiPlanets/Views/PlanetsPage.xaml.cs
+++ MauiPlanets/Views/PlanetsPage.xaml.cs
@@ -2,8 +2,17 @@ namespace MauiPlanets.Views;
 
 public partial class PlanetsPage : ContentPage
 {
+	private const uint AnimationDuration = 800u;
+
 	public PlanetsPage()
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+
+    }
 }
