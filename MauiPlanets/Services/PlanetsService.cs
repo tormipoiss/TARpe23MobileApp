87da6d08c0900b17a27659d8a147908cdecfd80b
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-02-11 14:03:29 +0200

    updated planetsservice and planetspage frontend, backend

--- MauiPlanets/Services/PlanetsService.cs
+++ MauiPlanets/Services/PlanetsService.cs
@@ -14,8 +14,32 @@ namespace MauiPlanets.Services
         {
             new()
             {
-
+                Name = "Mercury",
+                Subtitle = "mercury.png",
+                Description = "Mercury is the first planet from the Sun and the smallest in the Solar System. In English, it is named after the ancient Roman god Mercurius (Mercury), god of commerce and communication, and the messenger of the gods. Mercury is classified as a terrestrial planet, with roughly the same surface gravity as Mars. The surface of Mercury is heavily cratered, as a result of countless impact events that have accumulated over billions of years.",
+                AccentColorStart = Color.FromArgb("#353535"),
+                AccentColorEnd = Color.FromArgb("#8d9098"),
+                Images = new()
+                {
+                    "https://cdn.theatlantic.com/thumbor/D15rQggf6357X1-u6VpTD2N1yQE=/0x27:1041x613/976x549/media/img/mt/2017/04/MercuryImage/original.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/73_carousel_mercury_2.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/75_mercury_carousel_1.jpg"
+                }
             }
         };
+
+        public static List<Planet> GetFeaturedPlanets()
+        {
+            var random = new Random();
+            var randomizePlanets = planets
+                .OrderBy(item => random.Next());
+
+            return randomizePlanets
+                .Take(2)
+                .ToList();
+        }
+
+        public static List<Planet> GetAllPlanets()
+            => planets;
     }
 }
--- MauiPlanets/Views/PlanetsPage.xaml.cs
+++ MauiPlanets/Views/PlanetsPage.xaml.cs
@@ -1,3 +1,6 @@
+using MauiPlanets.Models;
+using MauiPlanets.Services;
+
 namespace MauiPlanets.Views;
 
 public partial class PlanetsPage : ContentPage
@@ -13,6 +16,24 @@ public partial class PlanetsPage : ContentPage
     {
         base.OnAppearing();
 
-
+		lstPopularPlanets.ItemsSource = PlanetsService.GetFeaturedPlanets();
+		lstAllPlanets.ItemsSource = PlanetsService.GetAllPlanets();
     }
+
+	async void Planets_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
+	{
+		await Navigation.PushAsync(new PlanetsDetailsPage(e.CurrentSelection.First() as Planet));
+	}
+
+	async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+	{
+		await CloseMenu();
+	}
+
+	private async Task CloseMenu()
+	{
+		_ = MainContentGrid.FadeTo(1, AnimationDuration);
+		_ = MainContentGrid.ScaleTo(1, AnimationDuration);
+		await MainContentGrid.TranslateTo(0, 0, AnimationDuration, Easing.CubicIn);
+	}
 }
