57a6c60c6628aa3291c8a7dd3be8514ab186ec91
tormi <160420092+askatskov@users.noreply.github.com>
2025-02-14 14:08:16 +0200

    updated planetsdetailspage

--- MauiPlanets/Views/PlanetsDetailsPage.xaml.cs
+++ MauiPlanets/Views/PlanetsDetailsPage.xaml.cs
@@ -10,4 +10,9 @@ public partial class PlanetsDetailsPage : ContentPage
 
 		this.BindingContext = planet;
 	}
+
+	async void BackButton_Clicked(System.Object sender, System.EventArgs e)
+	{
+		await Navigation.PopAsync();
+	}
 }
