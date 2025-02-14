3db94e62119a75de53e7cbd04eabc410b330fd46
Opilane TTHK <opilane@tthk.ee>
2025-02-14 12:55:25 +0200

    Added profilepic thing to planetpage

--- MauiPlanets/Views/PlanetsPage.xaml.cs
+++ MauiPlanets/Views/PlanetsPage.xaml.cs
@@ -17,13 +17,20 @@ public partial class PlanetsPage : ContentPage
         base.OnAppearing();
 
 		lstPopularPlanets.ItemsSource = PlanetsService.GetFeaturedPlanets();
-		lstAllPlanets.ItemsSource = PlanetsService.GetAllPlanets();
+		//lstAllPlanets.ItemsSource = PlanetsService.GetAllPlanets();
     }
 
 	async void Planets_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
 	{
 		await Navigation.PushAsync(new PlanetsDetailsPage(e.CurrentSelection.First() as Planet));
 	}
+
+	async void ProfilePic_Clicked(System.Object sender, System.EventArgs e)
+	{
+		_ = MainContentGrid.TranslateTo(-this.Width * 0.5, this.Height * 0.1, AnimationDuration, Easing.CubicIn);
+		await MainContentGrid.ScaleTo(0.8, AnimationDuration);
+		_ = MainContentGrid.ScaleTo(0.8, AnimationDuration);
+    }
 
 	async void GridArea_Tapped(System.Object sender, System.EventArgs e)
 	{
