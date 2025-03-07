b81a94955468684017cbf5ce3ebc12348f04e30e
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-03-07 15:28:42 +0200

    added controls for spacing, added offer, added offers section in homepage

--- FruitVegBasketMaui/Controls/Spacer.xaml.cs
+++ FruitVegBasketMaui/Controls/Spacer.xaml.cs
@@ -2,8 +2,18 @@ namespace Controls;
 
 public partial class Spacer : ContentView
 {
-	public Spacer()
-	{
-		InitializeComponent();
-	}
+    public Spacer()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly BindableProperty SpaceProperty =
+        BindableProperty.Create(nameof(Space), typeof(int), typeof(Spacer), defaultValue: 10);
+
+    public int Space
+    {
+        get => (int)GetValue(SpaceProperty);
+        set => SetValue(SpaceProperty, value);
+    }
+
 }
